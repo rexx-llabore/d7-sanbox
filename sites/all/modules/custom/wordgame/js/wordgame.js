d39a23394330490915dfe16a5a74282ff2ab21a8
Rexx Llabore <rexx@appnovation.com>
2014-10-20 21:28:54 -0700

    wordgame initial implementation

--- sites/all/modules/custom/wordgame/js/wordgame.js
+++ sites/all/modules/custom/wordgame/js/wordgame.js
@@ -1,9 +1,71 @@
 (function ($) {
   Drupal.behaviors.wordGame = {
-  attach: function (context, settings) {
-    alert('hello world');
-  }
-};
+    attach: function (context, settings) {
+      
+      var wordList = [
+        'bob',
+        'test',
+        'happy',
+        'asdf',
+        'qwer'
+      ];
+      var interval;
+      var userInputs = [];
+      
+      $('#word-link-start', context).click(function (e) {
+        $('#word', context).text(wordList.shift());
+        $('#wordgame-container', context).show();
+        $('#wordgame-container-start', context).hide();
+        
+        
+        interval = setInterval(function () {
+          if (wordList.length > 0) {
+            
+            // Display the next word on the list.
+            $('#word', context).text(wordList.shift());
+            
+            // Show the links again so users.
+            $('#wordgame-links', context).show('slow');
+          }
+          else {            
+            $('#wordgame-links').hide('slow');
+            
+            // Display user inputs on the screen.
+            if (Object.keys(userInputs).length > 0) {
+              for (var key in userInputs) {
+                $('#word', context).append(key + ':' + userInputs[key] + '</br>');
+              }
+            }
+            clearInterval(interval);
+          }
+        }, 3000);
+        
+        e.preventDefault();
+      });
+      
+      
+      // User clicks 'W'.
+      $('#word-link', context).click(function () {
+        
+        var currentWord = $('#word', context).html();
+        userInputs[currentWord] = 'word';
+        
+        // Hide the links when user has chosen an answer.
+        $('#wordgame-links', context).hide('slow');
+      });
+      
+      // User clicks 'N'.
+      $('#non-word-link', context).click(function () {
+        
+        var currentWord = $('#word', context).html();
+        userInputs[currentWord] = 'non-word';
+        
+        // Hide the links when user has chosen an answer.
+        $('#wordgame-links', context).hide('slow');
+      });
+      
+    }
+  };
 })(jQuery);
 
  
--- sites/all/modules/custom/wordgame/templates/wordgame.tpl.php
+++ sites/all/modules/custom/wordgame/templates/wordgame.tpl.php
@@ -1,9 +1,15 @@
- <div id="wordgame-container">
+<div id="wordgame-container" style="display: none"> 
   <div id="word-container">
     <span id="word">
       test
     </span>
-  </d>
-  <a id="word-link" href="#">W</a>
-  <a id="non-word-link" href="#">N</a>
+  </div>
+  <div id="wordgame-links">
+    <a id="word-link" href="#">W</a>
+    <a id="non-word-link" href="#">N</a>
+  </div>
+</div>
+
+<div id="wordgame-container-start">
+ <a id="word-link-start" href="#">Start</a>
 </div>
