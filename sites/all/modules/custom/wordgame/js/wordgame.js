ce8e77b8989f603c61c63d63181c0b3b50abad56
Rexx Llabore <rexx@appnovation.com>
2014-10-23 14:40:15 -0700

    v2 of the wordgame without the timer

--- sites/all/modules/custom/wordgame/js/wordgame.js
+++ sites/all/modules/custom/wordgame/js/wordgame.js
@@ -2,6 +2,34 @@
   Drupal.behaviors.wordGame = {
     attach: function (context, settings) {
       
+      /*
+       * Display the next text or the input results
+       */
+      function displayNext () {
+        if (wordList.length > 0) {
+            
+          // Display the next word on the list.
+          $('#word', context).text(wordList.shift());
+        }
+        else {
+          
+          // Hide the user input links.
+          $('#wordgame-links').hide();
+          
+          // Clear it first before appending.
+          $('#word', context).html('');
+          
+          // Display user inputs on the screen.
+          if (Object.keys(userInputs).length > 0) {
+            for (var key in userInputs) {
+              if ($.type(userInputs[key]) === 'string') {
+                $('#word', context).append(key + ': ' + userInputs[key] + '</br>');
+              }
+            }
+          }
+        }
+      }
+      
       var wordList = [
         'bob',
         'test',
@@ -9,64 +37,44 @@
         'asdf',
         'qwer'
       ];
-      var interval;
       var userInputs = [];
       
+      // Do something when the start link is clicked.
       $('#word-link-start', context).click(function (e) {
+        
+        // Display the first word.
         $('#word', context).text(wordList.shift());
+        
         $('#wordgame-container', context).show();
         $('#wordgame-container-start', context).hide();
-        
-        
-        interval = setInterval(function () {
-          if (wordList.length > 0) {
-            
-            // Display the next word on the list.
-            $('#word', context).text(wordList.shift());
-            
-            // Show the links again so users.
-            $('#wordgame-links', context).show('slow');
-          }
-          else {            
-            $('#wordgame-links').hide('slow');
-            
-            // Clear it first before appending.
-            $('#word', context).html('');
-            
-            // Display user inputs on the screen.
-            if (Object.keys(userInputs).length > 0) {
-              for (var key in userInputs) {
-                if ($.type(userInputs[key]) === 'string') {
-                  $('#word', context).append(key + ': ' + userInputs[key] + '</br>');
-                }
-              }
-            }
-            clearInterval(interval);
-          }
-        }, 3000);
-        
+      
         e.preventDefault();
       });
       
       
       // User clicks 'W'.
-      $('#word-link', context).click(function () {
+      $('#word-link', context).click(function (e) {
         
         var currentWord = $('#word', context).html();
         userInputs[currentWord] = 'word';
         
-        // Hide the links when user has chosen an answer.
-        $('#wordgame-links', context).hide('slow');
+        // Display the next text or the results.
+        displayNext();
+        
+        e.preventDefault();
       });
       
+      
       // User clicks 'N'.
-      $('#non-word-link', context).click(function () {
+      $('#non-word-link', context).click(function (e) {
         
         var currentWord = $('#word', context).html();
         userInputs[currentWord] = 'non-word';
         
-        // Hide the links when user has chosen an answer.
-        $('#wordgame-links', context).hide('slow');
+        // Display the next text or the results.
+        displayNext();
+        
+        e.preventDefault();
       });
       
     }
