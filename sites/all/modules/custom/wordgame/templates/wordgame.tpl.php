1cedb12eec479edadf6d04cb7ce3bc133103f38c
Rexx Llabore <rexx@appnovation.com>
2014-11-05 23:26:31 -0800

    themed wordgame

--- sites/all/modules/custom/wordgame/templates/wordgame.tpl.php
+++ sites/all/modules/custom/wordgame/templates/wordgame.tpl.php
@@ -1,15 +1,16 @@
-<div id="wordgame-container" style="display: none"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<div id="wordgame-container" style="display: none" class="text-center"> 
   <div id="word-container">
-    <span id="word">
+    <span id="word" style="font-size: 42px;">
       test
     </span>
   </div>
   <div id="wordgame-links">
-    <a id="word-link" href="#">W</a>
-    <a id="non-word-link" href="#">N</a>
+    <a id="word-link" href="#" class="btn btn-primary">W</a>
+    <a id="non-word-link" href="#" class="btn btn-primary">N</a>
   </div>
 </div>
 
-<div id="wordgame-container-start">
- <a id="word-link-start" href="#">Start</a>
+<div id="wordgame-container-start" class="text-center">
+ <a id="word-link-start" href="#" class="btn btn-primary btn-lg" style="color: #fff;">Start</a>
 </div>
